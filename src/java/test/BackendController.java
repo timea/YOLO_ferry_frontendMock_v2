27f547328414a9f4b19dd06410fd5cd4f0085480
Timea Kiss <k.timea.k@gmail.com>
2016-01-09 11:24:59 +0100

    The client to test the Bean.

---
+++ src/java/test/BackendController.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+
+import DTOs.CustomerDTO;
+import ETOs.NoTransactionETO;
+import contracts.AdminContract;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Timea Kiss hello@timeakiss.com
+ */
+@WebServlet(name = "BackendController", urlPatterns = {"/BackendController"})
+public class BackendController extends HttpServlet {
+
+    @EJB
+    private contracts.AdminContract backendBean;
+
+
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws ETOs.NoTransactionETO
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {      
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            CustomerDTO c = new CustomerDTO(firstName, lastName); 
+            CustomerDTO created = backendBean.createCustomer(c);
+            out.println("Customer["+created.getFirstName()+"] : " + created.getId());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            processRequest(request, response);
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            processRequest(request, response);
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+
+//    private AdminContract lookupBackendBeanRemote1() {
+//        try {
+//            Context c = new InitialContext();
+//            return (AdminContract) c.lookup("java:global/com.awesome_YOLO_ferry_backend_v2_ejb_1.0-SNAPSHOT/BackendBean!contracts.AdminContract");
+//        } catch (NamingException ne) {
+//            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+//            throw new RuntimeException(ne);
+//        }
+//    }
+
+}
